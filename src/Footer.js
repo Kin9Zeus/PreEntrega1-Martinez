1e20ab6abf1334a2246388e7868f7386fbc760f9
Kin9Zeus <aleckm99@hotmail.com>
2023-08-02 22:24:43 -0500

    Es

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,11 @@ function Footer () {
     return (
       <>
         <footer>
-        <div class="left">
+        <div class="iz">
         <p>&copy; 2023 Derechos reservados</p>
     </div>
 
-    <div class="right">
+    <div class="der">
         <p><a href="https://www.youtube.com/intl/ALL_es/howyoutubeworks/policies/community-guidelines/">Política de privacidad</a></p>
         <p><a href="https://www.facebook.com/legal/terms">Términos y condiciones</a></p>
     </div>
