4faf61f18a5f447dbb7f2b5e95a183ebef62753a
Kin9Zeus <aleckm99@hotmail.com>
2023-08-21 22:54:01 -0500

    Segunda Entrega React

--- src/CartWidget.js
+++ src/CartWidget.js
@@ -1,10 +1,14 @@
 
+import CartIcon from './Images/ShoppingCart.png'; //Importar images de fichero local
+import { useState } from 'react'; //Probando para usar Hook
+
 function CartWidget() {
-    const numero = 5;
+    //const numero = 5;
+    const [numero] = useState(3); //Cambiando a variable reactiva
   
     return (
       <div className="cart-widget">
-        <img src="https://assets.stickpng.com/images/59bedb1e7a216d0b052f128b.png" alt="Logo React JS"></img>
+        <img src={CartIcon} alt="Logo Chopping Cart"></img>
         <span className="cart-widget__notification">{numero}</span>
       </div>
     );
--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,11 @@ function Footer () {
     return (
       <>
         <footer>
-        <div class="iz">
+        <div className="iz">
         <p>&copy; 2023 Derechos reservados</p>
     </div>
 
-    <div class="der">
+    <div className="der">
         <p><a href="https://www.youtube.com/intl/ALL_es/howyoutubeworks/policies/community-guidelines/">Política de privacidad</a></p>
         <p><a href="https://www.facebook.com/legal/terms">Términos y condiciones</a></p>
     </div>
